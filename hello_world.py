30d7cabec416d585e1d56bfb7044d02c5487447d
TshegoM8 <letshegom8@gmail.com>
2025-09-02 22:01:26 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#Request for user's name 
+name = input("Enter your name: ")
+print(name)
+#now ask for age and print
+age = input("enter your your age: ")
+print(age)
+print("Hello,World!")
+
+
+
+
+
